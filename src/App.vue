1259a9687c978998b4691be31be8fab276cf603d
Adolfo Valdivieso <adoval4@gmail.com>
2019-11-07 10:59:53 -0300

    markup for registry view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1> {{ msg }} </h1>
+<template >
+  <div id="app" class="register-ctn columns is-vcentered">
+    <div class="column">
+      <img
+        class="logo"
+        src="https://static.platzi.com/media/user_upload/Page%201-d8d869f4-7130-4ae9-9907-2fa809bd136a.jpg"
+      >
+      <h1 class="title">
+        {{ title }}
+      </h1>
+      <form >
+        <div class="form-group">
+          <label for="">
+            Nombre
+          </label>
+          <input
+            type="text"
+            v-bind:value="user.first_name"
+          >
+        </div>
+        <div class="form-group">
+          <label for="">
+            Apellido
+          </label>
+          <input
+            type="text"
+            v-bind:value="user.last_name"
+          >
+        </div>
+        <div class="form-group">
+          <label for="">
+            Cargo
+          </label>
+          <input
+            type="text"
+            v-bind:value="user.job_title"
+          >
+        </div>
+        <div class="form-group btn-container">
+          <button id="send-btn" type="button" name="button" class="btn-primary">
+            ENVIAR
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -10,12 +51,51 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello world'
+      title: 'Registro de estación',
+      user: {
+        first_name: 'Adolfo',
+        last_name: 'Valdivieso',
+        job_title: 'CTO'
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+
+  body, #app {
+    height: 100%;
+    min-height: 100vh;
+  }
+
+  .columns.is-vcentered {
+    -webkit-box-align: center;
+       -ms-flex-align: center;
+          align-items: center;
+  }
+
+  .register-ctn {
+    padding: 35px;
+    width: 450px;
+    margin-left: auto;
+    margin-right: auto;
+
+    img.logo {
+      width: 100%;
+    }
+
+    .title {
+      margin-top: 30px;
+      margin-bottom: 30px;
+      text-align: center;
+      color: $app-blue;
+    }
+
+    .btn-container {
+      text-align: right;
+    }
+  }
+
 </style>
