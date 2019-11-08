e345dd90565e1a3a2777f62342a00cd84aa70bc1
Adolfo Valdivieso <adoval4@gmail.com>
2019-11-08 11:40:51 -0300

    initial daily bot dev

--- src/App.vue
+++ src/App.vue
@@ -1,133 +1,126 @@
 <template >
-  <div id="app" class="register-ctn columns is-vcentered">
-    <div class="column">
-      <img
-        class="logo"
-        src="https://static.platzi.com/media/user_upload/Page%201-d8d869f4-7130-4ae9-9907-2fa809bd136a.jpg"
-      >
-      <ul class="navbar">
-        <li class="nav-item">
-          <a
-            class="btn-link nav-btn"
-            href="javascript: void(0);"
-            v-on:click="onSwitchFormToSignin"
-          >
-            Registrate
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="btn-link nav-btn"
-            href="javascript: void(0);"
-            v-on:click="onSwitchFormToLogin"
-          >
-            Ingresa
-          </a>
-        </li>
-      </ul>
-      <h1 class="title">
-        <span v-show="signin">
-          Registro de estación
-        </span>
-        <span v-show="!signin">
-          Ingresa a tu estación
-        </span>
-      </h1>
-      <form v-if="signin" >
-        <div class="form-group">
-          <label for="">
-            Email
-          </label>
-          <input
-            type="email"
-            v-model="email"
-          >
-        </div>
-        <div class="form-group">
-          <label for="">
-            Nombre
-          </label>
-          <input
-            type="text"
-            v-model="first_name"
-          >
-        </div>
-        <div class="form-group">
-          <label for="">
-            Apellido
-          </label>
-          <input
-            type="text"
-            v-model="last_name"
-          >
-        </div>
-        <div class="form-group">
-          <label for="">
-            Cargo
-          </label>
-          <input
-            type="text"
-            v-model="job_title"
-          >
-        </div>
-        <div class="form-group btn-container">
-          <span v-if="full_name">
-            Bienvenido {{ full_name }}!
+  <div id="app">
+    <section class="section">
+      <nav class="nav has-shadow">
+        <div class="container">
+          <span class="title">
+            Daily Bot
           </span>
-          <button id="send-btn" type="button" name="button" class="btn-primary">
-            Crear
-          </button>
-        </div>
-      </form>
-      <form v-if="!signin" >
-        <div class="form-group">
-          <label for="">
-            Email
-          </label>
-          <input
-            type="email"
-            v-model="email"
+          <a
+            class="button"
+            v-on:click="onCallBtnClick"
           >
+            Llamar
+          </a>
+          <a
+
+            class="button"
+            v-bind:class="{
+              'is-info': !running,
+              'is-link': (running && !time_is_over),
+              'is-danger': (running && time_is_over)
+            }"
+            v-on:click="onTimerBtnClick"
+          >
+            {{ time_left_str }}
+          </a>
+          <a class="button is-success">
+            Enviar
+          </a>
         </div>
-        <div class="form-group btn-container">
-          <button id="send-btn" type="button" name="button" class="btn-primary">
-            Entrar
-          </button>
+      </nav>
+      <div class="container main-container">
+        <div class="columns">
+          <div class="column">
+            <div class="">
+              <h1>Ayer</h1>
+            </div>
+            <div class="field">
+              <div class="control">
+                <textarea
+                  class="textarea is-primary"
+                  rows="20"
+                  v-model="past_data"
+                ></textarea>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
-    </div>
+        <div class="columns">
+          <div class="column">
+            <div class="">
+              <h1>Hoy</h1>
+            </div>
+            <div class="field">
+              <div class="control">
+                <textarea
+                  class="textarea is-primary"
+                  rows="20"
+                  v-model="today_data"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+// const SLACK_CHANNEL_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const SLACK_CHANNEL_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const MEET_LINK = "https://meet.google.com/vmo-yxuk-eok";
+const MINUTES = 20;
+const INTERVAL_MS = 100;
+
 export default {
   name: 'app',
   data () {
     return {
-      title: 'Registro de estación',
-      signin: true,
-      email: '',
-      first_name: '',
-      last_name: '',
-      job_title: ''
+      current_time_ms: MINUTES*60*1000,
+      running: false,
+      time_passed_ms: 0,
+      past_data: '',
+      today_data: ''
     }
   },
+  mounted: function() {
+    setInterval(()=>{
+      if(this.running) {
+        this.current_time_ms -= INTERVAL_MS;
+      }
+    }, INTERVAL_MS);
+  },
   computed: {
-    full_name: function() {
-      return `${this.first_name}  ${this.last_name}`
+    time_left_str: function() {
+      const sign = this.current_time_ms >= 0 ? '' : '-';
+      const time_left = Math.abs(this.current_time_ms)
+      const minutes = Math.floor(time_left / 60/1000);
+      const seconds = Math.floor(time_left/1000) - minutes * 60;
+
+      const minutes_str = minutes >= 10 ? minutes : `0${minutes}`;
+      const seconds_str = seconds >= 10 ? seconds : `0${seconds}`;
+
+      return `${sign}${minutes_str}:${seconds_str}`;
+    },
+    time_is_over: function() {
+      return this.current_time_ms <= 0;
     }
   },
   methods: {
-    onSwitchFormToSignin: function() {
-      this.signin = true;
+    onTimerBtnClick: function() {
+      this.running = !this.running;
     },
-    onSwitchFormToLogin: function() {
-      this.signin = false;
-    }
-  },
-  watch: {
-    email: function(new_val, past_val) {
-      console.log(new_val, past_val)
+    onCallBtnClick: function() {
+      axios.post(
+        SLACK_CHANNEL_WEBHOOK_URL,
+        JSON.stringify({
+          text: `<!channel> daily: ${MEET_LINK}`
+        })
+      )
     }
   }
 }
@@ -142,32 +135,9 @@ export default {
     color: $app-black;
   }
 
-  .columns.is-vcentered {
-    -webkit-box-align: center;
-       -ms-flex-align: center;
-          align-items: center;
+  .main-container {
+    margin-top: 20px;
   }
 
-  .register-ctn {
-    padding: 35px;
-    width: 450px;
-    margin-left: auto;
-    margin-right: auto;
-
-    img.logo {
-      width: 100%;
-    }
-
-    .title {
-      margin-top: 30px;
-      margin-bottom: 30px;
-      text-align: center;
-      color: $app-blue;
-    }
-
-    .btn-container {
-      text-align: right;
-    }
-  }
 
 </style>
---
+++ src/components/button_timer.vue
@@ -0,0 +1,20 @@
+<template >
+  <a
+    href="#"
+    class="button"
+    v-bind:class="{ is-primary: !running  }"
+  >
+    Llamar
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
