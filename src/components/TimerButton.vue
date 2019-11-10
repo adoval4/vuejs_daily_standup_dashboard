fc3e8b99b00af1913f630eeb4d39be82e25e648e
Adolfo Valdivieso <adoval4@gmail.com>
2019-11-10 11:29:49 -0300

    Timer button component

---
+++ src/components/TimerButton.vue
@@ -0,0 +1,57 @@
+<template >
+  <a
+    class="button"
+    v-bind:class="{
+      'is-info': !running,
+      'is-link': (running && !time_is_over),
+      'is-danger': (running && time_is_over)
+    }"
+    v-on:click="onClick"
+  >
+    Time: {{ time_left_str }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: [
+    'total_time_ms'
+  ],
+  data () {
+    return {
+      time_passed_ms: 0,
+      running: false,
+      interval_ms: 100
+    }
+  },
+  mounted: function() {
+    setInterval(()=>{
+      if(this.running) {
+        this.time_passed_ms += this.interval_ms;
+      }
+    }, this.interval_ms);
+  },
+  computed: {
+    time_left_str: function() {
+      const time_left = this.total_time_ms - this.time_passed_ms;
+      const sign = time_left >= 0 ? '' : '-';
+      const time_left_abs = Math.abs(time_left);
+      const minutes = Math.floor(time_left_abs/60/1000);
+      const seconds = Math.floor(time_left_abs/1000) - minutes * 60;
+
+      const minutes_str = minutes >= 10 ? minutes : `0${minutes}`;
+      const seconds_str = seconds >= 10 ? seconds : `0${seconds}`;
+
+      return `${sign}${minutes_str}:${seconds_str}`;
+    },
+    time_is_over: function() {
+      return this.current_time_ms <= 0;
+    }
+  },
+  methods: {
+    onClick: function() {
+      this.running = !this.running;
+    }
+  }
+}
+</script>
